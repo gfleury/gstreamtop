44d56854ec665d048900dd988fa626fb85e4448b
gfleury <gfleury@gmail.com>
2018-12-17 11:26:47 +0100

    Adding cmdline query option

--- cmd.go
+++ cmd.go
@@ -56,6 +56,44 @@ func main() {
 		},
 	}
 
+	var cmdRunQuery = &cobra.Command{
+		Use:   "runQuery mappingname \"SELECT COUNT(*) FROM table GROUP BY field\"",
+		Short: "Run a SQL query.",
+		Long:  `runQuery runs a SQL queries against text streams.`,
+		Args:  cobra.MinimumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			for i, mapping := range c.Mappings {
+				if mapping.Name == args[0] {
+					err := o.CreateStreamFromConfigurationMapping(&c.Mappings[i], nil)
+					if err != nil {
+						fmt.Println(err)
+						os.Exit(1)
+					}
+					err = o.Stream().Query(args[1])
+					if err != nil {
+						fmt.Println(err)
+						os.Exit(1)
+					}
+					o.Configure()
+					break
+				}
+			}
+			if o == nil {
+				fmt.Printf("No mapping named %s found.\n", args[0])
+				os.Exit(1)
+			}
+			i, err := input.CreateStreamInputFromStreamOutput(o)
+			tables := o.Stream().GetTables()
+			i.SetTable(tables[0])
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			i.Run(inputFd)
+			o.Loop()
+		},
+	}
+
 	var rootCmd = &cobra.Command{Use: "app"}
 
 	cobra.OnInitialize(func() {
@@ -99,6 +137,7 @@ func main() {
 	rootCmd.PersistentFlags().BoolVar(&profiling, "profile", false, "enable profiling for CPU and Memory with pprof")
 
 	rootCmd.AddCommand(cmdRunNamedQuery)
+	rootCmd.AddCommand(cmdRunQuery)
 
 	rootCmd.Execute()
 }
